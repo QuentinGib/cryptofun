1d2d9abe59c1c0be4f5553c870cbd1b3c7370fff
Quentin Gibon <quentingibon@wanadoo.fr>
2020-10-14 12:06:50 +0200

    Réglage bugs sur la navigation standard/burger, burger non fonctionel sur les pages autres que index

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const burger = document.querySelector('.burger');
-const mainnav= document.getElementById('mainnav')
+const mainnav= document.getElementById('burgernav')
 burger.addEventListener('click', () => {
     mainnav.classList.toggle('active');
 });
