3cff89287e15d66a3615ffd171b9a299cebf0e93
Quentin Gibon <quentingibon@wanadoo.fr>
2020-11-11 17:38:20 +0100

    maj token dernière séance

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,9 +2,16 @@ const fetch = require('isomorphic-fetch')
 const express = require('express')
 
 const currenciesRoutes = require('./currencies-routes.js')
+const authRoutes = require('./auth-routes.js')
+
+const verifyToken = require('../middlewares/verify-token.js')
 
 const router = new express.Router()
 
+router.use('/auth', authRoutes)
+
 router.use('/currencies', currenciesRoutes)
+
+router.use('/verifyToken', verifyToken)
 
 module.exports = router
