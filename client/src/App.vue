ac534f27b194721a8bb47036affb79e851b02e51
LucasLvy <lucaslevy10@gmail.com>
2020-11-27 00:52:44 +0100

    changement des router-link dans la barre de navigation

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,23 +2,23 @@
   <div>
     <div class="main-header">
       <header class="main-header-title">
-          <h1><router-link class="toplink" to="/" >CRYPTO FUN</router-link></h1>
+          <h1><router-link class="toplink" :to="{name: 'Home'}">CRYPTO FUN</router-link></h1>
              <div class="burger"></div>
       </header>
       <div class="main-nav" id="mainnav">
-        <router-link class="main-tab" to="/">Home</router-link>
+        <router-link class="main-tab" :to="{name: 'Home'}">Home</router-link>
         <router-link class="main-tab" :to="{name: 'comprendre'}">Comprendre</router-link>
-        <router-link class="main-tab" to="/graphiques">Graphiques</router-link>
-        <router-link class="main-tab" to="/porte_monnaie">Porte Monnaie</router-link>
-        <router-link class="main-tab" to="/a_propos">A propos</router-link>
+        <router-link class="main-tab" :to="{name: 'graphiques'}">Graphiques</router-link>
+        <router-link class="main-tab" :to="{name: 'porte_monnaie'}">Porte Monnaie</router-link>
+        <router-link class="main-tab" :to="{name: 'a_propos'}">A propos</router-link>
       </div>
     </div>
     <div class="burger-nav" id="burgernav">
-        <router-link tag="li" class="burger-tab" to="/" >Home</router-link>
+        <router-link tag="li" class="burger-tab" :to="{name: 'Home'}" >Home</router-link>
         <router-link tag="li" class="burger-tab" :to="{name: 'comprendre'}">Comprendre</router-link>
-        <router-link tag="li" class="burger-tab" to="/graphiques">Graphiques</router-link>
-        <router-link tag="li" class="burger-tab" to="/porte_monnaie">Porte Monnaie</router-link>
-        <router-link tag="li" class="burger-tab" to="/a_propos">A propos</router-link>
+        <router-link tag="li" class="burger-tab" :to="{name: 'graphiques'}">Graphiques</router-link>
+        <router-link tag="li" class="burger-tab" :to="{name: 'porte_monnaie'}">Porte Monnaie</router-link>
+        <router-link tag="li" class="burger-tab" :to="{name: 'a_propos'}">A propos</router-link>
       </div>
     <router-view/>
   </div>
