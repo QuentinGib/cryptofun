d87fc908c8ffb215087559529a30690d33dcafb5
Quentin Gibon <quentingibon@wanadoo.fr>
2020-12-06 16:50:32 +0100

    classieux

--- client/src/views/PorteMonnaie.vue
+++ client/src/views/PorteMonnaie.vue
@@ -11,32 +11,32 @@
 </head>
 <body>
     <main>
-      <div>
+      <div class="login-box">
+        <h2>Login</h2>
         <form @submit.prevent="sendCredentials">
-          <p>
-            <label for="username">
-              Username
-            </label>
+          <div class="user-box">
             <input
-              id="username"
-              v-model="username"
-              type="text"
-              placeholder="Username"
+            type="text"
+            name=""
+            required=""
+            id="username"
+            v-model="username"
+            autocomplete="off"
             >
-          </p>
-          <p>
-            <label for="password">
-              Password
-            </label>
+            <label for="username">Username</label>
+          </div>
+          <div class="user-box">
             <input
-              id="password"
-              v-model="password"
-              type="password"
-              required
-              placeholder="Password"
+            name=""
+            id="paswword"
+            v-model="password"
+            type="password"
+            required=""
+            autocomplete="off"
             >
-          </p>
-          <button type="submit">
+            <label>Password</label>
+          </div>
+          <button type="submit" class="subbutton">
             Se connecter
           </button>
         </form>
@@ -85,5 +85,106 @@ export default {
 </script>
 
 <style>
+
+.login-box {
+  font-family: sans-serif;
+  background: linear-gradient(#141e30, #243b55);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 400px;
+  padding: 40px;
+  transform: translate(-50%, -50%);
+  box-sizing: border-box;
+  box-shadow: 0 15px 25px rgba(0,0,0,.6);
+  border-radius: 10px;
+}
+
+.login-box h2 {
+  margin: 0 0 30px;
+  padding: 0;
+  color: #fff;
+  text-align: center;
+}
+
+.login-box .user-box {
+  position: relative;
+}
+
+.login-box .user-box input {
+  width: 100%;
+  padding: 10px 0;
+  font-size: 16px;
+  color: #fff;
+  margin-bottom: 30px;
+  border: none;
+  border-bottom: 1px solid #fff;
+  outline: none;
+  background: transparent;
+}
+.login-box .user-box label {
+  position: absolute;
+  top:0;
+  left: 0;
+  padding: 10px 0;
+  font-size: 16px;
+  color: #fff;
+  pointer-events: none;
+  transition: .5s;
+}
+
+.login-box .user-box input:focus ~ label,
+.login-box .user-box input:valid ~ label {
+  top: -20px;
+  left: 0;
+  color: #03e9f4;
+  font-size: 12px;
+}
+
+.login-box form a {
+  position: relative;
+  display: inline-block;
+  padding: 10px 20px;
+  color: #03e9f4;
+  font-size: 16px;
+  text-decoration: none;
+  text-transform: uppercase;
+  overflow: hidden;
+  transition: .5s;
+  margin-top: 40px;
+  letter-spacing: 4px
+}
+
+.subbutton {
+  border: 1.5px solid #fff;
+  font-size: 16px;
+  color: #fff;
+  height: 2.25rem;
+  line-height: 1.25rem;
+  margin-top: 0.25rem;
+  padding: 0.25rem 0.5rem;
+  width: 15rem;
+  transition: all 0.25s;
+  background:  #21364E;
+  margin-top: 0.625rem;
+  order: 4;
+  outline: 1px dashed transparent;
+  outline-offset: 2px;
+  padding-left: 0;
+  border-radius: 10px;
+}
+
+.subbutton:hover {
+  color: #03e9f4;
+  border: 1.5px solid #03e9f4;
+  background:  #21364E;
+}
+
+.subbutton:active {
+  color: #21364E;
+  border: 1.5px solid #03e9f4;
+  background: #03e9f4;
+  transition: all 0.125s;
+}
 
 </style>
--- client/src/views/Wallet.vue
+++ client/src/views/Wallet.vue
@@ -5,80 +5,95 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Porte Monnaie</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300i,400" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
     <link rel="stylesheet" href="./css/main.css">
     <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
 </head>
 <body>
-    <main class="wallet-main">
-      <div class="wallet-block1">
-        <div class="wallet-sommeTotale">
-          <h2>Valeur totale</h2>
-          <p>{{sommeTotale}}$</p>
-        </div>
-        <div class="wallet-monnaieDispo">
-          <h2>Monnaie disponible</h2>
-          <p>{{ holdingDolls }}$</p>
-        </div>
+  <div class="container">
+    <div class="card">
+      <h3 class="title">
+        Valeur totale<br>
+        <br><br><br>{{sommeTotale}} $
+      </h3>
+      <div class="bar">
+        <div class="emptybar"></div>
+        <div class="filledbar"></div>
       </div>
-      <div class="wallet-block2">
-        <div class="wallet-cryptoDispo">
-          <h2>Cryptos disponibles</h2>
-          <ul>
-            <li v-for="hold in holdingsNotNull" :key="hold.id">
-              <p>{{ hold.name }}</p>
-              <p>{{ hold.somme }}</p>
-            </li>
-          </ul>
-        </div>
-        <div class="wallet-AchatRevente">
-          <h2>Trading :</h2>
-          <ul>
-            <form @submit.prevent="acheter">
-              <select v-model="cryptoSelected">
-                <option
-                  v-for="(currency, idx) in currencies"
-                  v-bind:key="currency.id + idx"
-                  :value="currency.name"
-                >
-                  {{ currency.name }}
-                </option>
-              </select>
-              <p>
-                <label for="achat">
-                  pour un montant de $
-                </label>
-                <input
-                  id="achat"
-                  v-model.number="achat"
-                  type="number"
-                >
-              </p>
-              <button type="submit">Acheter</button>
-            </form>
-            <form @submit.prevent="vendre">
-              <select v-model="cryptoSelected">
-                <option v-for="currency in holdingsNotNull" v-bind:key="currency.id" >
-                  {{ currency.name }}
-                </option>
-              </select>
-              <p>
-                <label for="vente">
-                  pour un montant de $
-                </label>
-                <input
-                  id="vente"
-                  v-model.number="vente"
-                  type="number"
-                >
-              </p>
-              <button type="submit">Vendre</button>
-            </form>
-          </ul>
-        </div>
+    </div>
+    <div class="card">
+      <h3 class="title">
+        Monnaie disponible<br>
+        <br><br>{{ holdingDolls }} $
+      </h3>
+      <div class="bar">
+        <div class="emptybar"></div>
+        <div class="filledbar"></div>
       </div>
-    </main>
+    </div>
+    <div class="card">
+      <h3 class="title">Cryptos disponibles</h3>
+      <ul class="cryptodispo">
+        <li v-for="hold in holdingsNotNull" :key="hold.id">
+          <p>{{ hold.name }}  {{ hold.somme }}</p>
+        </li>
+      </ul>
+      <div class="circle">
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <circle class="stroke" cx="30" cy="30" r="20"/>
+      </svg>
+      </div>
+    </div>
+  </div>
+  <div class="wallet-block2">
+    <div class="wallet-AchatRevente">
+      <h2>Trading :</h2>
+      <ul>
+        <form @submit.prevent="acheter" class="form-achat">
+          <select v-model="cryptoSelected">
+            <option
+              v-for="(currency, idx) in currencies"
+              v-bind:key="currency.id + idx"
+              :value="currency.name"
+            >
+              {{ currency.name }}
+            </option>
+          </select>
+          <p class="text-achat">
+            <label for="achat">
+              pour un montant de $
+            </label>
+            <input
+              id="achat"
+              v-model.number="achat"
+              type="number"
+            >
+          </p>
+          <button type="submit">Acheter</button>
+        </form>
+        <form @submit.prevent="vendre">
+          <select v-model="cryptoSelected">
+            <option v-for="currency in holdingsNotNull" v-bind:key="currency.id" >
+              {{ currency.name }}
+            </option>
+          </select>
+          <p>
+            <label for="vente">
+              pour un montant de $
+            </label>
+            <input
+              id="vente"
+              v-model.number="vente"
+              type="number"
+            >
+          </p>
+          <button type="submit">Vendre</button>
+        </form>
+      </ul>
+    </div>
+  </div>
 </body>
 </html>
 </template>
@@ -104,7 +119,7 @@ export default {
   mounted () {
     const token = localStorage.getItem('token')
     this.login = localStorage.getItem('login')
-    this.holdingDolls = localStorage.getItem('HoldingDollsOf' + this.login) || 1000
+    this.holdingDolls = JSON.parse(localStorage.getItem('HoldingDollsOf' + this.login)) || 1000
     this.sommeTotale = this.holdingDolls
     this.currencies = JSON.parse(localStorage.getItem('prices'))
 
@@ -144,7 +159,7 @@ export default {
           sum += this.currencies.find(x => x.id === cryptoHolded.id).price * cryptoHolded.somme
         }
         console.log(this.sommeTotale)
-        this.sommeTotale = sum
+        this.sommeTotale = Math.round(sum * 1000) / 1000
       })
       .catch(error => { this.error = error })
   },
@@ -181,5 +196,101 @@ export default {
 </script>
 
 <style>
+.container {
+  font-family: 'Open Sans', sans-serif;
+  position: absolute;
+  height: 500px;
+  width: 1000px;
+  top: 150px;
+  left: 20%;
+  display: flex;
+}
 
+.card {
+  display: flex;
+  height: 420px;
+  width: 300px;
+  background-color: #17141d;
+  border-radius: 10px;
+  box-shadow: -1rem 0 3rem #000;
+  transition: 0.4s ease-out;
+  position: relative;
+  left: 0px;
+}
+
+.card:not(:first-child) {
+    margin-left: -50px;
+}
+
+.card:hover {
+  transform: translateY(-20px);
+  transition: 0.4s ease-out;
+}
+
+.card:hover ~ .card {
+  position: relative;
+  left: 50px;
+  transition: 0.4s ease-out;
+}
+
+.title {
+  color: white;
+  font-weight: 300;
+  position: absolute;
+  left: 20px;
+  top: 15px;
+}
+
+.bar {
+  position: absolute;
+  top: 300px;
+  left: 20px;
+  height: 10px;
+  width: 250px;
+}
+
+.emptybar {
+  background-color: #2e3033;
+  width: 100%;
+  height: 100%;
+}
+
+.filledbar {
+  position: absolute;
+  top: 0px;
+  z-index: 3;
+  width: 0px;
+  height: 100%;
+  background: rgb(0,154,217);
+  background: linear-gradient(90deg, rgba(0,154,217,1) 0%, rgba(217,147,0,1) 65%, rgba(255,186,0,1) 100%);
+  transition: 0.6s ease-out;
+}
+
+.card:hover .filledbar {
+  width: 210px;
+  transition: 0.4s ease-out;
+}
+
+.circle {
+  position: absolute;
+  top: 350px;
+  left: calc(80% - 20px);
+}
+
+.stroke {
+  stroke: white;
+  stroke-dasharray: 360;
+  stroke-dashoffset: 360;
+  transition: 0.6s ease-out;
+}
+
+svg {
+  fill: #17141d;
+  stroke-width: 1.7px;
+}
+
+.card:hover .stroke {
+  stroke-dashoffset: 260;
+  transition: 0.6s ease-out;
+}
 </style>
