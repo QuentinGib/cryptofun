1b21452a193e6c6d631e0bce3c66334aa3dc48a6
LucasLvy <lucaslevy10@gmail.com>
2021-03-15 18:45:02 +0100

    ajout mongo-db

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,15 @@ const port = 4000
 app.use(bodyParser.json())
 
 app.use('/api/v1', router)
+getConnectionToMongoDB().then(({db})=> {
+  insertDocuments(db,(err)=>{
+    if (err){
+      console.error(err)
+    }
+    console.log('OK inserted')
+  })
 
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
