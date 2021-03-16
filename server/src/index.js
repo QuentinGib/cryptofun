c4acafe506772245d2e313e06627a4e12cdc1ca9
Quentin Gibon <quentingibon@wanadoo.fr>
2021-03-16 14:57:21 +0100

    maj cours 16/03

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv')
 const express = require('express')
 const bodyParser = require('body-parser')
+const Connection = require('./connect.js')
 
 const router = require('./routes/index.js')
 
@@ -13,15 +14,12 @@ const port = 4000
 app.use(bodyParser.json())
 
 app.use('/api/v1', router)
-getConnectionToMongoDB().then(({db})=> {
-  insertDocuments(db,(err)=>{
-    if (err){
-      console.error(err)
-    }
-    console.log('OK inserted')
-  })
 
-})
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+Connection.getConnection()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`)
+    })
+  }).catch(error => {
+    console.error(error)
+  })
