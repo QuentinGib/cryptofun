a412a651ac60ab3c7c104256b678d21e9b83291f
Quentin Gibon <quentingibon@wanadoo.fr>
2021-03-23 16:30:47 +0100

    netlify

--- client/src/api/api-routes.js
+++ client/src/api/api-routes.js
@@ -1,6 +1,7 @@
+const API_HOST = process.env.VUE_APP_API_HOST
 export default {
-  login: '/api/v1/auth/token',
-  trade: '/api/v1/compte/cryptoTrade',
-  currencies: '/api/v1/currencies',
-  me: '/api/v1/auth/me'
+  login: API_HOST + '/api/v1/auth/token',
+  trade: API_HOST + '/api/v1/compte/cryptoTrade',
+  currencies: API_HOST + '/api/v1/currencies',
+  me: API_HOST + '/api/v1/auth/me'
 }
